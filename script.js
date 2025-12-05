453e7399ceee0ed2a138e30af21858254ed23165
Under450 <caj@me.com>
2025-12-05 19:17:54 +0000

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ function toggleMenu() {
     document.getElementById("mobileNav").classList.toggle("open");
 }
 
-document.querySelectorAll(".title").forEach(button => {
-    button.addEventListener("click", () => {
-        const content = button.nextElementSibling;
+document.querySelectorAll(".accordion .title").forEach(btn => {
+    btn.addEventListener("click", () => {
+        const content = btn.nextElementSibling;
         content.style.display = content.style.display === "block" ? "none" : "block";
     });
 });
